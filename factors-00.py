c18b1cd855f5053b4d1823f9422a9f0a51fbdc96
Lain0x0 <lainyamato03@gmail.com>
2024-02-02 15:25:13 +0100

    factors-00.py

--- factors-00.py
+++ factors-00.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
 from sys import argv
+
+
 def factorize(number):
     factors = []
-  if (number % 2 == 0):
-      factor = number // 2
-    factors.append((2, factor))
-else:
-    for n in range(3, number, 2):
-        if (number % i == 0):
-            factor = number // n
-        factors.append((n, factor))
-        break
-  return (factors)
-if __name__ == "__main__":
+
+    if number % 2 == 0:
+        factor = number // 2
+        factors.append((2, factor))
+    else:
+        for n in range(3, number, 2):
+            if (number % n == 0):
+                factor = number // n
+                factors.append((n, factor))
+                break
+    return factors
+
+
+if (__name__ == "__main__"):
+
     if (len(argv) != 2):
+
         print("Usage: factors <file>")
-  else:
-      with open(argv[1]) as f:
-          for line in f:
-              num = int(line)
-        result = factorize(num)
-        for factor in result:
-            print("{}={}*{}".format(num, factor[0], factor[1]))
+    else:
+        with open(argv[1]) as f:
+            for line in f:
+                num = int(line)
+                result = factorize(num)
+                for factor in result:
+                    print("{}={}*{}".format(num, factor[0], factor[1]))

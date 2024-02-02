b09cf9b6bc29bf90ddeb1f1da808d703543dd401
Lain0x0 <lainyamato03@gmail.com>
2024-02-02 15:29:19 +0100

    factors

---
+++ factors_00.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from sys import argv
+
+with open(argv[1]) as f:
+    for line_1 in f:
+        number = int(line_1)
+        print("{:d}=".format(number), end="")
+        if (number % 2 == 0):
+            print("{}*2".format(number//2))
+            continue
+        for k in range(3, number, 2):
+            if (number % k == 0):
+                factor = number//k
+                for j in range(3, factor, 2):
+                    if (factor % j == 0 or k % j == 0):
+                        break
+                print("{}*{}".format(factor, k))
+                break
